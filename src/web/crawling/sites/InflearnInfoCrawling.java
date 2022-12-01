a8ab0e7c874b588b7d186b7d3eb6827ebcf38932
Kitae's Github <aqua0405@gmail.com>
2022-12-02 08:54:54 +0900

    인프런 추가

--- src/web/crawling/sites/InflearnInfoCrawling.java
+++ src/web/crawling/sites/InflearnInfoCrawling.java
@@ -23,23 +23,65 @@ public class InflearnInfoCrawling {
                 System.out.println(element);
 //                System.out.println(element.attr("abs:src"));
             }
-			
+
+			// 강의 제목
 			Elements titleElements = document.select("div.card-content > div.course_title");
 			for (Element element : titleElements) {
 				System.out.println("강의 제목 : " + element.text());
 			}
 			
+			// 가격
 			Elements priceElements = document.getElementsByClass("price");
 			
 			for (int i = 0; i < priceElements.size(); i++) {
 				final String price = priceElements.get(i).text();
-				System.out.println("가격: " + price);
+				System.out.println("price : " + price);
+				
+				final String realPrice = getRealPrice(price);
+				final String salePrice = getSalePrice(price);
+				
+				final int realIntPrice = toInt(removeNotNumeric(realPrice));
+				final int saleIntPrice = toInt(removeNotNumeric(salePrice));
+				
+				System.out.println("가격 : " + realIntPrice);
+				System.out.println("할인 가격 : " + saleIntPrice);
 			}
 			
+			// 강의 링크 
+			Elements linkElements = document.select("a.course_card_front");
+			for (int j = 0; j < linkElements.size(); j++) {
+                final String url = linkElements.get(j).attr("abs:href");
+                System.out.println("url : " + url);
+            }
+			
+			// 평점 
 			
 		} catch (IOException e) {			
 			e.printStackTrace();
 		}
+	}
+
+	/**
+	 * 숫자를 문자로 변환
+	 * @param str
+	 * @return
+	 */
+	private static int toInt(final String str) {		
+		return Integer.parseInt(str);
+	}
+
+	private static String removeNotNumeric(final String str) {	
+		return str.replaceAll("\\W", "");
+	}
+
+	private static String getRealPrice(final String price) {
+		final String[] pricesArray = price.split(" ");
+        return pricesArray[0];
+	}
+
+	private static String getSalePrice(final String price) {
+		final String[] pricesArray = price.split(" ");
+        return (pricesArray.length == 1) ? price : pricesArray[1];
 	}
 
 }
