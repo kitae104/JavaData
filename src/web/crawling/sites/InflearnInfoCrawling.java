8cc07ead1d9f7f458c0fa8f3e3ae002ececb9fd8
Kitae's Github <aqua0405@gmail.com>
2022-12-02 01:36:30 +0900

    인프런 크롤링 일부

--- src/web/crawling/sites/InflearnInfoCrawling.java
+++ src/web/crawling/sites/InflearnInfoCrawling.java
@@ -15,11 +15,28 @@ public class InflearnInfoCrawling {
 		Connection conn = Jsoup.connect(inflearnUrl);
 		try {
 			Document document = conn.get();
+			
+			// 클래스 속성을 이용하여 이미지 URL 요소 추출 
 			Elements imageUrlElements = document.getElementsByClass("swiper-lazy");
 			
 			for (Element element : imageUrlElements) {
                 System.out.println(element);
+//                System.out.println(element.attr("abs:src"));
             }
+			
+			Elements titleElements = document.select("div.card-content > div.course_title");
+			for (Element element : titleElements) {
+				System.out.println("강의 제목 : " + element.text());
+			}
+			
+			Elements priceElements = document.getElementsByClass("price");
+			
+			for (int i = 0; i < priceElements.size(); i++) {
+				final String price = priceElements.get(i).text();
+				System.out.println("가격: " + price);
+			}
+			
+			
 		} catch (IOException e) {			
 			e.printStackTrace();
 		}
